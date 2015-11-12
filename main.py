6c7947db39688d1bbcd0a13eea99dafc5c758027
linkwin <alan@allteksystemsdesign.com>
2015-11-12 10:51:22 -0500

    code base

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def generate_puzzle():
             puzzle[x][y] = numbers[y + N * x]
     return puzzle
 
+def print_puzzle(puzzle):
+    for i in puzzle:
+        for j in i:
+            print repr(j).rjust(2),
+        print
 
 dirs = dict()
 puzzle = generate_puzzle()
@@ -29,29 +34,33 @@ for x in range(N):
     else:
         continue
     break
-print(curr)
-for i in range(50):
-    #while puzzle is not soln:
+
+#for i in range(50):
+while puzzle is not soln:
     fitness = [0 for i in range(N)]  # 0:left 1:up 2:right 3:down
     # Closer fitness is to zero the better
     left = (curr[0] - 1, curr[1])
     down = (curr[0], curr[1] + 1)
-    right = (curr[0] + 1, curr[1])
+    right = (curr[0]+1, curr[1])
     up = (curr[0], curr[1] - 1)
     dirs = [left, up, right, down]
 
-    if not curr[0] - 1 <= 0:  #left
+    if not curr[0] - 1 < 0:  #left
         fitness[0] = abs(puzzle[left[0]][left[1]] - soln[curr[0]][curr[1]])
-    if not curr[1] - 1 <= 0:  #up
+    if not curr[1] - 1 < 0:  #up
         fitness[1] = abs(puzzle[up[0]][up[1]] - soln[curr[0]][curr[1]])
     if not curr[0] + 1 >= N:  #right
         fitness[2] = abs(puzzle[right[0]][right[1]] - soln[curr[0]][curr[1]])
     if not curr[1] + 1 >= N:  #down
         fitness[3] = abs(puzzle[down[0]][down[1]] - soln[curr[0]][curr[1]])
-
-    probs = list()
+#--------------------------------------------
+    probs = list() #creates a probability distribution based on fitness of neighboring squares
     for i in range(N):
-        probs.append((float(sum(fitness)) - float(fitness[i])) / float(sum(fitness)))
+        if fitness[i] is not 0:
+            probs.append((float(sum(fitness)) - float(fitness[i])) / float(sum(fitness)))
+        else:
+            probs.append(0)
+
     rnd = random.random()
     best_greedy_stoc = 0
     prob_sum = 0
@@ -59,7 +68,9 @@ for i in range(50):
         prob_sum += prob
         if prob_sum >= rnd:
             best_greedy_stoc = fitness[probs.index(prob)]
+            break
 
+#---------------------------
     best_greedy = min(fitness)
 
     ind = fitness.index(best_greedy_stoc)
@@ -68,4 +79,5 @@ for i in range(50):
     puzzle[new_blank[0]][new_blank[1]] = 0
     curr = new_blank
 
-    print(puzzle)
+    print_puzzle(puzzle)
+    raw_input("(press enter)")
